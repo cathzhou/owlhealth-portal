6e932f41cdcbc772b78c5fa4fce25f24ee9f26c1
Anjali Mathew <anjalim.mathew@icloud.com>
2024-09-21 16:28:09 -0500

    login

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,19 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from "@mantine/core";
 import {navigate} from "@storybook/addon-links";
+import { AuthProvider } from "../contexts/authContext";
+import { useRoutes } from "react-router-dom";
+import Login from '../components/auth/login';
+
 import './LandingPage.css';
+import { HomePage } from './HomePage';
 
 
 export function LandingPage(): JSX.Element {
     const history = useNavigate();
 
+    
     return (
-        <div className="landing-page">
-            <h1>Welcome to Rice Student Health Portal</h1>
-            <p>Your one-stop solution for health services</p>
-            <Button variant="contained" color="primary">
-                Login with Google
-            </Button>
-        </div>
+        <AuthProvider> 
+            <div className="landing-page">
+                <h1>Welcome to Rice Student Health Portal</h1>
+                <p>Your one-stop solution for health services</p>
+                <Login />
+            </div>
+        </AuthProvider>
     );
 }
