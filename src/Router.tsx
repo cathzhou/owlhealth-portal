d7f8f092eec543e1e060d248aaa2be69f5834512
Catherine <catherineyaozhou@gmail.com>
2024-09-21 15:39:36 -0500

    Small router fix

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
+import { HomePage } from './pages/HomePage';
 
 export function Router(): JSX.Element {
   return (
